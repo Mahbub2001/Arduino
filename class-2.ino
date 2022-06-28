12c3cc063695462f625e28ba94fda6ac9f854a7d
Mahbub Ahmed Turza <102087572+Mahbub2001@users.noreply.github.com>
2022-06-29 04:01:19 +0600

    Add files via upload

---
+++ class-2.ino
@@ -0,0 +1,63 @@
+void setup() {
+     
+      pinMode(2,OUTPUT);
+      pinMode(3,OUTPUT);
+      pinMode(4,OUTPUT);
+      pinMode(5,OUTPUT);
+      pinMode(6,OUTPUT);
+      pinMode(7,OUTPUT);
+      pinMode(8,OUTPUT);
+      pinMode(9,OUTPUT);
+}
+
+void loop() {
+
+   digitalWrite(2,HIGH);
+   digitalWrite(3,HIGH);
+   delay(300);
+   digitalWrite(4,HIGH);
+   digitalWrite(5,HIGH);
+   delay(300);
+   digitalWrite(6,HIGH);
+   digitalWrite(7,HIGH);
+   delay(300);
+   digitalWrite(8,HIGH);
+   digitalWrite(9,HIGH);
+   delay(300);
+
+   digitalWrite(2,LOW);
+   digitalWrite(3,LOW);
+   digitalWrite(4,LOW);
+   digitalWrite(5,LOW);
+   digitalWrite(6,LOW);
+   digitalWrite(7,LOW);
+   digitalWrite(8,LOW);
+   digitalWrite(9,LOW);
+   delay(300);
+
+   digitalWrite(9,HIGH);
+   digitalWrite(8,HIGH);
+   delay(300);
+   digitalWrite(7,HIGH);
+   digitalWrite(6,HIGH);
+   delay(300);
+   digitalWrite(5,HIGH);
+   digitalWrite(4,HIGH);
+   delay(300);
+   digitalWrite(3,HIGH);
+   digitalWrite(2,HIGH);
+   delay(300);
+
+   digitalWrite(2,LOW);
+   digitalWrite(3,LOW);
+   digitalWrite(4,LOW);
+   digitalWrite(5,LOW);
+   digitalWrite(6,LOW);
+   digitalWrite(7,LOW);
+   digitalWrite(8,LOW);
+   digitalWrite(9,LOW);
+   delay(300);
+   
+
+
+}
