3f7fb1394685995ae2dd3f0276579e1fb14b1819
Mahbub Ahmed Turza <102087572+Mahbub2001@users.noreply.github.com>
2022-06-30 01:16:07 +0600

    Add files via upload

---
+++ assignment7_digital_.ino
@@ -0,0 +1,49 @@
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  pinMode(A7,INPUT);
+  pinMode(7,OUTPUT);
+
+}
+int a;
+
+void loop() {
+
+  a = digitalRead(A7);
+
+  if(a == 1)
+  {
+    lcd.print("Out of light");
+  }
+  else
+    {
+    lcd.print("You are in light"); 
+    }
+
+  if(a == 1)
+     {
+    for(int i = 0; i < 4; i++)
+    {
+      digitalWrite(7,HIGH);
+      delay(30);
+      digitalWrite(7,LOW);
+      delay(30);  
+    } 
+  }
+  else
+  {
+    for(int i = 0; i < 2; i++)
+    {
+      digitalWrite(7,HIGH);
+      delay(60);
+      digitalWrite(7,LOW);
+      delay(60);  
+     } 
+    }
+delay(1000);
+lcd.clear();  
+}
